14ba433c827be03e3dc31659279f7437e43c714e
Mathias Gebbe <mathias.gebbe@gmail.com>
2016-11-24 21:34:29 +0100

    added script and pics

---
+++ change_bg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+function changebg {
+for i in $(ls ./pic/*.png); do
+   feh --bg-scale $i
+   sleep 60
+done
+}
+
+
+while [ true ]
+do
+ changebg
+done
