010f528ad4a923a1089737771ff50fa3b6123b8a
Mathias Gebbe <mathias.gebbe@intevation.de>
2016-11-24 21:39:59 +0100

    Update change_bg.sh

--- change_bg.sh
+++ change_bg.sh
@@ -2,14 +2,12 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-
 function changebg {
 for i in $(ls ./pic/*.png); do
    feh --bg-scale $i
    sleep 60
 done
 }
-
 
 while [ true ]
 do
